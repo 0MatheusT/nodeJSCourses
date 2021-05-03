063b302d2f63f90593773f2041206039f2e27562
Matheus Tavares <matheus.tavares@mam.contab>
2021-05-03 10:33:59 -0300

    feat - The first app file running on node

---
+++ first-node-project/app.js
@@ -0,0 +1,14 @@
+function hello(name) {
+  console.log("Hello " + name)
+}
+
+
+hello("Matheus")
+
+/* To run this code in node
+Make sure that you have node.js installed
+on your machine
+
+open your terminal
+and execute the command
+node fileName */
