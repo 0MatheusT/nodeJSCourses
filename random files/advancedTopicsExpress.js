7cf670ab8b6af23b9a65ffd8f0808df7062707e6
Matheus <matheustavares1@live.com>
2021-06-01 09:46:19 -0300

    update: configuration file

--- random files/advancedTopicsExpress.js
+++ random files/advancedTopicsExpress.js
@@ -1,10 +1,10 @@
 //A custom middleware function
-
-const Joi = require('joi');
+const config = require('config') //import das configurações
+const helmet = require('helmet') //pacote de segurança.
+const morgan = require('morgan') // Log no console
+const Joi = require('joi'); 
 const express = require('express');
 const importedMiddlware = require('./middlewareImported')
-const helmet = require('helmet')
-const morgan = require('morgan')
 const app = express();
 
 
@@ -17,10 +17,20 @@ app.use(express.urlencoded( { extended: true }));
 //middleware que faz acessarmos uma pasta statica no root da aplicação.
 //Quando digitamos a url com o nome do arquivo, já vai direto. O middleware se encarrega de buscar a info.
 app.use(express.static('public'));
-
-
 app.use(helmet());
-app.use(morgan('tiny'));
+
+
+//Configuração
+console.log('Nome da Aplicação: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Server: ' + config.get('mail.password'));
+
+
+//Condiciona o uso do morgan somente no ambiente de desenvolvimento
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  console.log('Morgan em funcionamento.')
+}
 
 
 //Aqui está nosso middleware customizado.
